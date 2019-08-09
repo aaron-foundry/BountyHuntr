0eab6c058c8d2501436034985ad202b38a31bdbf
Aaron Free <aaronfree@Aarons-MacBook-Pro.local>
2019-08-09 11:01:47 -0500

    add header bar style to screen components

---
+++ constants/HeaderStyle.js
@@ -0,0 +1,16 @@
+
+const HEADER_BAR_TITLE = "BountyHuntr";
+const HEADER_BAR_STYLE = {
+  backgroundColor: '#49b481',
+};
+const HEADER_BAR_TITLE_STYLE = {
+  fontSize: 24,
+  fontWeight: 'bold',
+  color: 'white'
+};
+
+export {
+    HEADER_BAR_TITLE,
+    HEADER_BAR_STYLE,
+    HEADER_BAR_TITLE_STYLE
+};
--- screens/MealPlanningScreen.js
+++ screens/MealPlanningScreen.js
@@ -1,5 +1,10 @@
 import React from 'react';
 import styled from 'styled-components/native';
+import {
+    HEADER_BAR_TITLE,
+    HEADER_BAR_STYLE,
+    HEADER_BAR_TITLE_STYLE
+} from '../constants/HeaderStyle';
 
 
 const SuggestedRecipesText = styled.Text`
@@ -7,6 +12,12 @@ const SuggestedRecipesText = styled.Text`
 `;
 
 class MealPlanningScreen extends React.Component {
+    static navigationOptions = {
+        title: HEADER_BAR_TITLE,
+        headerStyle: HEADER_BAR_STYLE,
+        headerTitleStyle: HEADER_BAR_TITLE_STYLE,
+    };
+
     render() {
         return (
             <SuggestedRecipesText>{`Suggested Recipes`}</SuggestedRecipesText>
